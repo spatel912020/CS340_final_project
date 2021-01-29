1a1556691c5988a408ccf7b5e3c1dd98288dfc93
Shreyank Patel <spatel91@vols.utk.edu>
2021-01-29 13:13:11 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SongSort from './Song';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div>
+    <div className="title">
+      <h1>Song Merge Sort</h1>
+      <ul>
+        <li><a class="active" href="#login">Login</a></li>
+        <li><a href="#Creat account">Create</a></li>
+      </ul>
+    </div>
+    <SongSort/>
+  </div>,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
