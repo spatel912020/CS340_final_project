6c42300285a9d7f71752636ba2a8c6626ba4f725
Shreyank Patel <spatel91@vols.utk.edu>
2021-03-20 22:59:50 -0400

    add final-project folder to git

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SongSort from './Song';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <div>
-    <div className="title">
-      <h1>Song Merge Sort</h1>
-      <div className= "user_account">
-        <ul>
-          <li><a class="active" href="#login">Login</a></li>
-          <li><a href="#Creat account">Create</a></li>
-        </ul>
-      </div>
-      <div className= "mode_switch">
-        <ul>
-          <li><a class="active" href="#Spotify">Spotify</a></li>
-          <li><a href="#Youtube">Youtube</a></li>
-        </ul>
-      </div>
-    </div>
-    <SongSort/>
-  </div>,
+    <App/>,
   document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
